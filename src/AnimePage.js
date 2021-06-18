f799d05fd1db829d21c4bd82e094fea43cd2705f
Rakib Khan <rakibkhan@live.co.uk>
2021-06-18 02:43:30 +0100

    .

--- src/AnimePage.js
+++ src/AnimePage.js
@@ -24,40 +24,45 @@ export default function AnimePage({ match }) {
 
     return (
         <div className="anime-page">
-            <div className="video-info">
-                <div className="title">
-                    <Typography variant="h4">{anime.title}</Typography>
+            <div className="bg"></div>
+            <div className="anime-page-content">
+                <div className="video-info">
+                    <div className="title">
+                        <Typography variant="h4">{anime.title}</Typography>
+                    </div>
+                    <div className="video-section">
+                        <iframe src={anime.trailer_url} className="video" />
+                    </div>
+                    <div className="about">
+                        <Typography variant="body1">
+                            {anime.synopsis}
+                        </Typography>
+                    </div>
                 </div>
-                <div className="video-section">
-                    <iframe src={anime.trailer_url} className="video" />
-                </div>
-                <div className="about">
-                    <Typography variant="body1">{anime.synopsis}</Typography>
-                </div>
-            </div>
-            <div className="img-extra-info">
-                <div className="image">
-                    <img src={anime.image_url} alt="" />
-                </div>
-                <div className="eng-title">
-                    <Typography variant="h6">
-                        EN Title: {anime.title_english}
-                    </Typography>
-                </div>
-                <div className="jp-title">
-                    <Typography variant="h6">
-                        JP Title: {anime.title_japanese}
-                    </Typography>
-                </div>
-                <div className="no-ep">
-                    <Typography variant="h6">
-                        No of Episodes: {anime.episodes}
-                    </Typography>
-                </div>
-                <div className="finished">
-                    <Typography variant="h6">
-                        Finished: {anime.airing ? "No" : "Yes"}
-                    </Typography>
+                <div className="img-extra-info">
+                    <div className="image">
+                        <img src={anime.image_url} alt="" />
+                    </div>
+                    <div className="eng-title">
+                        <Typography variant="h6">
+                            EN Title: {anime.title_english}
+                        </Typography>
+                    </div>
+                    <div className="jp-title">
+                        <Typography variant="h6">
+                            JP Title: {anime.title_japanese}
+                        </Typography>
+                    </div>
+                    <div className="no-ep">
+                        <Typography variant="h6">
+                            No of Episodes: {anime.episodes}
+                        </Typography>
+                    </div>
+                    <div className="finished">
+                        <Typography variant="h6">
+                            Finished: {anime.airing ? "No" : "Yes"}
+                        </Typography>
+                    </div>
                 </div>
             </div>
         </div>
